1a1af847534ac8c3b1a0e639d0c79eaedada7ffa
AnnaKupyniak <annakupinak770@gmail.com>
2022-04-25 23:45:59 +0300

    add float

--- basics_types/int.py
+++ basics_types/int.py
@@ -3,3 +3,9 @@ first_number = int(input("first number:" ))
 second_number = int(input("second number:" ))
 
 print("result:", first_number + second_number)
+
+first_number = float(input("first float number:" ))
+
+second_number = float(input("second float number:" ))
+
+print("result:", first_number + second_number)
